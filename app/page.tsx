4dd6099975c49208cc1af36525c9a7ce073f76a5
Syed Ibrahim Ali <sp23bscs0178@maju.edu.pk>
2025-02-16 22:18:33 +0500

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,27 @@
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import RecentPorject from "@/components/RecentPorject";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import { FaHome } from "react-icons/fa";
+import { navItems } from "@/data";
+import Clients from "@/components/ui/Cleint";
+
+// import Approach from "@/components/Approach";
+import Footer from "@/components/ui/Footer";
+import Experience from "@/components/Experience";
 
 export default function Home() {
   return (
-   <main className="relative bg-black-100 flex justify-center items-center overflow-hiddencmx-auto sm:px-10 px-5">
+   <main className="relative bg-black-100 flex justify-center items-center overflow-hidden mx-auto sm:px-10 px-5">
     <div className="max-w-7xl w-full">
-      <FloatingNav navItems={[{name: 'Home', link: '/', icon: <FaHome/>}]}/>
+      <FloatingNav navItems={navItems}/>
       <Hero/>
       <Grid/>
+      <RecentPorject/>
+      <Clients/>
+      <Experience/>
+      {/* <Approach/> */}
+      <Footer/>
     </div>
    </main>
   );
--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,10 +1,15 @@
-// import { useState } from "react";
+"use client";
+import { useState } from "react";
 import { IoCopyOutline } from "react-icons/io5";
-import Lottie from "react-lottie";
-import { cn } from "@/utils/cn";
-import { BackgroundGradientAnimation } from "./GradientBg";
 
-// import animationData from "@/data/confetti.json";
+// Also install this npm i --save-dev @types/react-lottie
+import Lottie from "lottie-react";
+
+import { cn } from "@/utils/cn";
+
+import { BackgroundGradientAnimation } from "./GradientBg";
+// import GridGlobe from "./GridGlobe";
+import animationData from "@/data/confetti.json";
 import MagicButton from "./MagicButton";
 
 export const BentoGrid = ({
@@ -16,10 +21,11 @@ export const BentoGrid = ({
 }) => {
   return (
     <div
-      className={
-        "grid grid-cols-1 md:grid-cols-6 lg:grid-cols-5 md:grid-row-7 gap-4 lg:gap-8 mx-auto " +
+      className={cn(
+        // change gap-4 to gap-8, change grid-cols-3 to grid-cols-5, remove md:auto-rows-[18rem], add responsive code
+        "grid grid-cols-1 md:grid-cols-6 lg:grid-cols-5 md:grid-row-7 gap-4 lg:gap-8 mx-auto",
         className
-      }
+      )}
     >
       {children}
     </div>
@@ -31,6 +37,7 @@ export const BentoGridItem = ({
   id,
   title,
   description,
+  //   remove unecessary things here
   img,
   imgClassName,
   titleClassName,
@@ -48,46 +55,49 @@ export const BentoGridItem = ({
   const leftLists = ["ReactJS", "Express", "Typescript"];
   const rightLists = ["VueJS", "NuxtJS", "GraphQL"];
 
-  // const [copied, setCopied] = useState(false);
+  const [copied, setCopied] = useState(false);
 
-  // const defaultOptions = {
-  //   loop: copied,
-  //   autoplay: copied,
-  //   animationData: animationData,
-  //   rendererSettings: {
-  //     preserveAspectRatio: "xMidYMid slice",
-  //   },
-  // };
+  const defaultOptions = {
+    loop: copied,
+    autoplay: copied,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
 
-  // const handleCopy = () => {
-  //   const text = "hsu@jsmastery.pro";
-  //   navigator.clipboard.writeText(text);
-  //   setCopied(true);
-  // };
+  const handleCopy = () => {
+    const text = "aryanaj2255@gmail.com";
+    navigator.clipboard.writeText(text);
+    setCopied(true);
+  };
 
   return (
     <div
-      className={
-        "row-span-1 relative overflow-hidden rounded-3xl border border-white/[0.1] group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4 " +
+      className={cn(
+        // remove p-4 rounded-3xl dark:bg-black dark:border-white/[0.2] bg-white  border border-transparent, add border border-white/[0.1] overflow-hidden relative
+        "row-span-1 relative overflow-hidden rounded-3xl border border-white/[0.1] group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4",
         className
-      }
+      )}
       style={{
+        //   add these two
+        //   you can generate the color from here https://cssgradient.io/
         background: "rgb(4,7,29)",
         backgroundColor:
           "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
       }}
     >
+      {/* add img divs */}
       <div className={`${id === 6 && "flex justify-center"} h-full`}>
         <div className="w-full h-full absolute">
           {img && (
             <img
               src={img}
               alt={img}
-              className={imgClassName + " object-cover object-center "}
+              className={cn(imgClassName, "object-cover object-center ")}
             />
           )}
         </div>
-        
         <div
           className={`absolute right-0 -bottom-5 ${
             id === 5 && "w-full opacity-80"
@@ -97,34 +107,40 @@ export const BentoGridItem = ({
             <img
               src={spareImg}
               alt={spareImg}
+              //   width={220}
               className="object-cover object-center w-full h-full"
             />
           )}
         </div>
-          <div>
-            {id===6 &&(
-              <BackgroundGradientAnimation>
-                <div className="absolute z-50 justify-center items-center text-white"/>
-              </BackgroundGradientAnimation>
-            )}
-          </div>
+        {id === 6 && (
+          // add background animation , remove the p tag
+          <BackgroundGradientAnimation>
+            {/* <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl"></div> */}
+          </BackgroundGradientAnimation>
+        )}
+
         <div
-          className={
-            titleClassName +
-            " group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
-          }
+          className={cn(
+            titleClassName,
+            "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
+          )}
         >
+          {/* change the order of the title and des, font-extralight, remove text-xs text-neutral-600 dark:text-neutral-300 , change the text-color */}
           <div className="font-sans font-extralight md:max-w-32 md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10">
             {description}
           </div>
-          <div className="font-sans text-lg lg:text-3xl max-w-96 font-bold z-10">
+          {/* add text-3xl max-w-96 , remove text-neutral-600 dark:text-neutral-300*/}
+          {/* remove mb-2 mt-2 */}
+          <div
+            className={`font-sans text-lg lg:text-3xl max-w-96 font-bold z-10`}
+          >
             {title}
           </div>
 
-          
-
+          {/* Tech stack list div */}
           {id === 3 && (
             <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
+              {/* tech stack lists */}
               <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
                 {leftLists.map((item, i) => (
                   <span
@@ -149,6 +165,35 @@ export const BentoGridItem = ({
                   </span>
                 ))}
               </div>
+            </div>
+          )}
+          {id === 6 && (
+            <div className="mt-5 relative">
+              {/* button border magic from tailwind css buttons  */}
+              {/* add rounded-md h-8 md:h-8, remove rounded-full */}
+              {/* remove focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 */}
+              {/* add handleCopy() for the copy the text */}
+              <div
+                className={`absolute -bottom-5 right-0 ${
+                  copied ? "block" : "block"
+                }`}
+              >
+                {/* <img src="/confetti.gif" alt="confetti" /> */}
+                <Lottie
+                  animationData={animationData}
+                  loop={copied}
+                  autoplay={copied}
+                  style={{ height: 200, width: 400 }}
+                />
+              </div>
+
+              <MagicButton
+                title={copied ? "Email is Copied!" : "Copy my email address"}
+                icon={<IoCopyOutline />}
+                position="left"
+                handleClick={handleCopy}
+                otherClasses="!bg-[#161A31]"
+              />
             </div>
           )}
         </div>
